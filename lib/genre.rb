bcf4b165d84b2e6942bc4925622c82d9ac5a01c6
Elijah wines <elijahwines84@yahoo.com>
2020-03-17 13:12:58 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,7 +1,7 @@
 require_relative './concerns/findable.rb'
 
 class Genre
-  extend Concerns::Findable 
+  extend Concerns::Findable
 
   attr_accessor :name, :songs
   @@all = []
@@ -10,36 +10,36 @@ class Genre
     @name = name
     @songs = []
   end
-  # accepts a name for the new genre 
-  # creates a 'songs' collection 
+  # accepts a name for the new genre
+  # creates a 'songs' collection
 
   def self.all
     @@all
   end
-  # returns the @@all class variable 
+  # returns the @@all class variable
 
   def self.destroy_all
     @@all = []
   end
-  # clears the @@all class variable 
+  # clears the @@all class variable
 
   def save
     @@all << self
   end
-  # adds the Genre instance to the @@all class variable 
+  # adds the Genre instance to the @@all class variable
 
   def self.create(name)
     g = self.new(name)
     g.save
     g
   end
-  #nitializes and saves the genre 
+  #nitializes and saves the genre
 
   def artists
     @songs.map { |m| m.artist }.uniq
   end
-  # returns a collection of artists for all of the genres songs 
-  # does not returnaduplicate artists if the genre has more than one song by a particular artist 
+  # returns a collection of artists for all of the genres songs
+  # does not returnaduplicate artists if the genre has more than one song by a particular artist
   # collects artists through its songs instead of maintaining its own @artsts instance variable 
 
 end
