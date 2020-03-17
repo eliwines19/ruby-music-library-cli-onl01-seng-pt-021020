92e3e0c836c285c6ba2bfd5420f8985ce7418ec0
Elijah wines <elijahwines84@yahoo.com>
2020-03-17 12:56:20 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -36,4 +36,55 @@ class MusicLibraryController
       end
     end
   end
+
+  def list_songs
+    Song.all.sort_by{|s| s.name}.each.with_index(1) do |x, i| 
+      puts "#{i}. #{x.artist.name} - #{x.name} - #{x.genre.name}"
+    end
+  end
+
+  def list_artists
+    Artist.all.sort_by{|s| s.name}.each.with_index(1) do |x, i| 
+      puts "#{i}. #{x.name}"
+    end
+  end
+
+  def list_genres
+    Genre.all.sort_by{|s| s.name}.each.with_index(1) do |x, i| 
+      puts "#{i}. #{x.name}"
+    end
+  end
+
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    input = gets.strip
+
+    if artist = Artist.find_by_name(input)
+      artist.songs.sort_by{ |x| x.name }.each.with_index(1) do |s, i|
+        puts "#{i}. #{s.name} - #{s.genre.name}"
+      end
+    end
+  end
+
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets.strip
+
+    if genre = Genre.find_by_name(input)
+      genre.songs.sort_by{ |x| x.name }.each.with_index(1) do |s, i|
+        puts "#{i}. #{s.artist.name} - #{s.name}"
+      end
+    end
+  end
+
+  def play_song
+    puts "Which song number would you like to play?"
+    input = gets.strip.to_i
+    if (1..Song.all.length).include?(input)
+      song = Song.all.sort_by{ |x| x.name }[input - 1]
+    end
+
+    puts "Playing #{song.name} by #{song.artist.name}" if song
+  end
+  
 end
