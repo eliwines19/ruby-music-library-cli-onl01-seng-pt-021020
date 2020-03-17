496e82565fc00cfddd8fa8474b80f4d7e18b8c6e
Elijah wines <elijahwines84@yahoo.com>
2020-03-17 12:44:27 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,44 @@
+class Artist
+
+  attr_accessor :name, :genres
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    a = self.new(name)
+    a.save
+    a
+  end
+
+  def add_song(song)
+    if !@songs.include?(song)
+      @songs << song
+      song.artist = self if song.artist.nil?
+    end
+  end
+
+  def songs
+    @songs
+  end
+
+  def genres
+    @songs.map { |m| m.genre }.uniq
+  end
+
+end
---
+++ lib/genre.rb
@@ -0,0 +1,33 @@
+class Genre
+
+  attr_accessor :name, :songs
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    g = self.new(name)
+    g.save
+    g
+  end
+
+  def artists
+    @songs.map { |m| m.artist }.uniq
+  end
+
+end
